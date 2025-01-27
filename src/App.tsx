8796f8993b2ead5520d35bab0b35adc8bad3caf6
samora <samora.sm.sam@gmail.com>
2025-01-27 21:17:41 +0200

    app.tsx updated

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import Loading from "./components/ui/Loading";
 import { ErrorBoundary } from "./components/ui/ErrorBoundary";
 import ErrorFallback from "./components/ui/ErrorFallback";
 import Drawer from "./components/ui/Drawer";
+import MovieDetail from "./components/MovieDetails";
 
 const Quiz = lazy(() => import("./pages/Quiz"));
 const Movies = lazy(() => import("./pages/Movies"));
@@ -18,12 +19,13 @@ function App() {
       >
         <div className="flex min-h-screen bg-gray-100">
           <Drawer />
-          <main className="flex-1 ml-0 lg:ml-64 min-h-screen">
+          <main className="flex-1 ml-0 lg:ml-64 pt-16 lg:pt-6">
             <Suspense fallback={<Loading />}>
-              <div className="p-6 flex items-center justify-center min-h-screen">
+              <div className="p-4 lg:p-6 flex items-start justify-center min-h-screen">
                 <Routes>
                   <Route path="/quiz" element={<Quiz />} />
                   <Route path="/movies" element={<Movies />} />
+                  <Route path="/movies/:id" element={<MovieDetail />} />
                   <Route path="/" element={<Navigate to="/quiz" replace />} />
                 </Routes>
               </div>
