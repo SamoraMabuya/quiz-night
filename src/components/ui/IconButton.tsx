e3ae2fef738b1ea95f3df30c6fcbbcde9e958178
samora <samora.sm.sam@gmail.com>
2025-01-28 21:58:27 +0200

    icon button component added

---
+++ src/components/ui/IconButton.tsx
@@ -0,0 +1,22 @@
+import { ButtonHTMLAttributes, ReactNode } from "react";
+import Button from "./Button";
+
+interface IconButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  icon: ReactNode;
+  label: string; // Make label required for accessibility
+}
+
+export function IconButton({
+  icon,
+  label,
+  className,
+  ...props
+}: IconButtonProps) {
+  return (
+    <Button className={className} label={label} {...props}>
+      <span className={"w-6 h-6 text-gray-600"} aria-hidden="true">
+        {icon}
+      </span>
+    </Button>
+  );
+}
