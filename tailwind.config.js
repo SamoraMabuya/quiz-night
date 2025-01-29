e32ea9a20a301c6f19a696442afb42a983ed146f
Samora Mabuya <samora@kriterion.ai>
2025-01-29 15:56:07 +0200

    tailwind.config.js updated, unuused colors removed

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,16 +5,13 @@ module.exports = {
     extend: {
       colors: {
         primary: {
-          50: "#f0f9ff",
-          100: "#e0f2fe",
           500: "#3b82f6",
           600: "#2563eb",
         },
-        secondary: {
-          50: "#f8fafc",
-          100: "#f1f5f9",
-          500: "#64748b",
-          600: "#475569",
+        dark: {
+          100: "#1f2937",
+          200: "#111827",
+          300: "#0f172a",
         },
       },
       borderRadius: {
